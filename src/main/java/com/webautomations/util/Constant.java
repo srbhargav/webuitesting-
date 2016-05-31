28aa5f4a1f0c370d794117e83236189680f7a0fb
Bhargav S R <bhargavsr@gmail.com>
2016-05-31 15:12:03 -0400

    update after small changes

--- src/main/java/com/webautomations/util/Constant.java
+++ src/main/java/com/webautomations/util/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 
 	 public static final String Username = "qatester1271";
 	 
-    public static final String Password = "mindtree@2016";
+    public static final String Password = "Mindtree@2020";
 
 	 public static final String eMail = "qatester1271@gmail.com";
 	
